817043a720417b8190b92959f5643b9c5a5b6cd0
Charles Teague <charles@merdianlabs.ai>
2025-11-07 18:20:22 -0500

    InfoPanel

--- src/inspect_scout/_view/www/src/app/result/info/InfoPanel.tsx
+++ src/inspect_scout/_view/www/src/app/result/info/InfoPanel.tsx
@@ -1,41 +1,66 @@
 import clsx from "clsx";
-import { FC, ReactNode } from "react";
+import { FC } from "react";
 
+import { Card, CardBody, CardHeader } from "../../../components/Card";
 import { LabeledValue } from "../../../components/LabeledValue";
-import { MetaDataGrid } from "../../../content/MetaDataGrid";
 import { RecordTree } from "../../../content/RecordTree";
 import { ModelUsage, ModelUsage2 } from "../../../types/log";
 import { ModelTokenTable } from "../../../usage/ModelTokenTable";
 import { ScannerData } from "../../types";
 
 import styles from "./InfoPanel.module.css";
+import { formatNumber } from "../../../utils/format";
 
 interface InfoPanelProps {
   result?: ScannerData;
 }
 
 export const InfoPanel: FC<InfoPanelProps> = ({ result }) => {
-  const panels: ReactNode[] = [<ScannerInfoPanel result={result} />];
-  panels.push(
-    <ModelTokenTable
-      model_usage={
-        result?.scanModelUsage as unknown as ModelUsage2 | ModelUsage
-      }
-    />
+  return (
+    result && (
+      <div className={clsx(styles.container)}>
+        <Card>
+          <CardHeader label="Scanner Info" type="modern" />
+          <CardBody>
+            <ScannerInfoPanel result={result} />
+          </CardBody>
+        </Card>
+        <Card>
+          <CardHeader label="Model Usage" type="modern" />
+          <CardBody>
+            <ModelTokenTable
+              model_usage={
+                result?.scanModelUsage as unknown as ModelUsage2 | ModelUsage
+              }
+            />
+          </CardBody>
+        </Card>
+        {result?.scanMetadata &&
+          Object.keys(result.scanMetadata).length > 0 && (
+            <Card>
+              <CardHeader label="Metadata" type="modern" />
+              <CardBody>
+                <RecordTree
+                  id={`scan-metadata-${result?.uuid}`}
+                  record={result?.scanMetadata || {}}
+                />
+              </CardBody>
+            </Card>
+          )}
+      </div>
+    )
   );
-
-  panels.push(<MetaDataGrid entries={result?.scanMetadata || {}} />);
-
-  return result && <div>{panels}</div>;
 };
 
 export const ScannerInfoPanel: FC<InfoPanelProps> = ({ result }) => {
   return (
-    <div>
+    <div className={clsx("text-size-small")}>
       <div className={clsx(styles.scanInfo)}>
         <LabeledValue label="Name">{result?.scannerName}</LabeledValue>
         <LabeledValue label="File">{result?.scannerFile}</LabeledValue>
-        <LabeledValue label="Tokens">{result?.scanTotalTokens}</LabeledValue>
+        <LabeledValue label="Tokens">
+          {result?.scanTotalTokens ? formatNumber(result.scanTotalTokens) : ""}
+        </LabeledValue>
       </div>
       {result?.scanTags && result.scanTags.length > 0 && (
         <LabeledValue label="Tags">

9a1c3ce3ea6f44d8870d0e830187bcc0adb5e26f
J.J. Allaire <jj.allaire@gmail.com>
2025-11-23 21:30:15 -0500

    don't create intermediate call

--- examples/database/weave/hooks.py
+++ examples/database/weave/hooks.py
@@ -1,5 +1,5 @@
 import os
-from contextlib import _GeneratorContextManager
+from contextlib import ExitStack
 
 import weave
 from inspect_ai.hooks import (
@@ -13,8 +13,6 @@ from inspect_ai.hooks import (
     hooks,
 )
 from inspect_ai.log import EvalSpec
-from pydantic_core import to_jsonable_python
-from weave.trace.call import Call
 from weave.trace.weave_client import WeaveClient
 
 
@@ -23,9 +21,7 @@ class WeaveHooks(Hooks):
     def __init__(self) -> None:
         self._client: WeaveClient | None = None
         self._evals: dict[str, EvalSpec] = {}
-        self._threads: dict[
-            str, tuple[_GeneratorContextManager[weave.ThreadContext, None, None], Call]
-        ] = {}
+        self._threads: dict[str, ExitStack] = {}
 
     def enabled(self) -> bool:
         return (
@@ -51,30 +47,25 @@ class WeaveHooks(Hooks):
     async def on_sample_start(self, data: SampleStart) -> None:
         assert self._client
 
-        # create and enter thread
-        thread_ctx = weave.thread(thread_id=data.sample_id)
-        thread_ctx.__enter__()
-
-        # create call
-        call_name = (
-            f"{self._evals[data.eval_id].task} ({data.summary.id}-{data.summary.epoch})"
-        )
-        call = self._client.create_call(
-            call_name,
-            inputs={"input": to_jsonable_python(data.summary.input)},
-            attributes={
-                "id": data.summary.id,
-                "epoch": data.summary.epoch,
-                "sample_metadata": data.summary.metadata,
-            },
+        # create exit stack
+        exit_stack = ExitStack()
+        exit_stack.__enter__()
+        exit_stack.enter_context(weave.thread(thread_id=data.sample_id))
+        exit_stack.enter_context(
+            weave.attributes(
+                {
+                    "id": data.summary.id,
+                    "epoch": data.summary.epoch,
+                    "sample_metadata": data.summary.metadata,
+                }
+            )
         )
 
-        # track thread and call for cleanup
-        self._threads[data.sample_id] = (thread_ctx, call)
+        # track exit stack for cleanup
+        self._threads[data.sample_id] = exit_stack
 
     async def on_sample_end(self, data: SampleEnd) -> None:
         assert self._client
 
-        thread_ctx, call = self._threads.pop(data.sample_id)
-        self._client.finish_call(call, output=data.sample.output.model_dump())
-        thread_ctx.__exit__(None, None, None)
+        exit_stack = self._threads.pop(data.sample_id)
+        exit_stack.__exit__(None, None, None)

1e897b8fef0b38f6e991f0b85e906eaa4431c7e9
Eric Patey <eric.patey@gmail.com>
2025-12-16 15:42:02 -0500

    Add /transcripts-dir and /transcripts endpoints to view server.

--- src/inspect_scout/_view/_server_common.py
+++ src/inspect_scout/_view/_server_common.py
@@ -3,6 +3,7 @@ from typing import Any
 from fastapi.responses import JSONResponse
 from inspect_ai._util.json import to_json_safe
 from typing_extensions import override
+from upath import UPath
 
 
 class InspectPydanticJSONResponse(JSONResponse):
@@ -11,3 +12,16 @@ class InspectPydanticJSONResponse(JSONResponse):
     @override
     def render(self, content: Any) -> bytes:
         return to_json_safe(content)
+
+
+async def _default_transcripts_dir() -> str:
+    return (
+        UPath(await _scrounged_transcripts_dir() or "file://./transcripts")
+        .resolve()
+        .as_posix()
+    )
+
+
+async def _scrounged_transcripts_dir() -> str | None:
+    # TODO: Scrounge any scan results to find a plausible transcript dir
+    return None

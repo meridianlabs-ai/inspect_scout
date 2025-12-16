ac38b1a7138128bcc45ed9cef8b71f0c3fc027e7
Eric Patey <eric.patey@gmail.com>
2025-12-16 17:38:31 -0500

    Add debugging to help diagnose transcripts_dir issue.

--- src/inspect_scout/_view/_api_v2.py
+++ src/inspect_scout/_view/_api_v2.py
@@ -119,6 +119,7 @@ def v2_api_app(
         request_transcripts_dir: str | None = Query(None, alias="dir"),
     ) -> list[TranscriptInfo]:
         transcripts_dir = request_transcripts_dir or await default_transcripts_dir()
+        print(f"/transcripts got\n\t{request_transcripts_dir=}\n\t{transcripts_dir=}")
         try:
             async with transcripts_from(transcripts_dir).reader() as tr:
                 return [t async for t in tr.index()]

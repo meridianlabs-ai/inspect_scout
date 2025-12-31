f601570b5870349797bc9a7e1b500d70fe633ff4
Charles Teague <charles@merdianlabs.ai>
2025-12-31 14:14:44 -0500

    fully purge index.ts types

--- src/inspect_scout/_view/www/src/types/index.ts
+++
@@ -1,8 +0,0 @@
-// Query builder
-export {
-  transcriptColumns,
-  Column,
-  ConditionBuilder,
-  TranscriptColumns,
-} from "../query";
-export type { ConditionModel, ScalarValue } from "../query";

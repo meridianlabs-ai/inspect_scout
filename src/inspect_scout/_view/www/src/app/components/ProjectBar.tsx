6d2761f9b758808d09074eba343c6a3b195b9294
J.J. Allaire <jj.allaire@gmail.com>
2026-01-12 15:17:22 -0500

    project button at right

--- src/inspect_scout/_view/www/src/app/components/ProjectBar.tsx
+++ src/inspect_scout/_view/www/src/app/components/ProjectBar.tsx
@@ -1,9 +1,11 @@
 import { FC } from "react";
 
 import { AppConfig } from "../../types/api-types";
+import { ApplicationIcons } from "../../components/icons";
 
 import styles from "./ProjectBar.module.css";
 import { appAliasedPath } from "../server/useConfig";
+import { ToolButton } from "./ToolButton";
 
 interface ProjectBarProps {
   config: AppConfig;
@@ -13,13 +15,16 @@ export const ProjectBar: FC<ProjectBarProps> = ({ config }) => {
   return (
     <div className={styles.projectBar}>
       <div className={styles.row}>
-        <span className={styles.left}>
+        <span className={styles.center}>
           {appAliasedPath(config, config.project_dir)}
         </span>
-        <span className={styles.right}>{config.project.transcripts ?? ""}</span>
-      </div>
-      <div className={styles.row}>
-        <span>{config.project.model ?? ""}</span>
+        <div className={styles.right}>
+          <ToolButton
+            label="Project"
+            icon={ApplicationIcons.config}
+            className={styles.projectButton}
+          />
+        </div>
       </div>
     </div>
   );

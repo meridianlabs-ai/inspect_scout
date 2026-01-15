b6d4f1280721ca4239f56cc3390fd815d50ba2c4
Charles Teague <charles@merdianlabs.ai>
2026-01-15 17:33:09 -0500

    Properly export the v1 server api for the time being
    v1 of the API will be removed within a few weeks, so this is temporary

--- src/inspect_scout/_view/www/src/index.ts
+++ src/inspect_scout/_view/www/src/index.ts
@@ -3,6 +3,8 @@ export { App } from "./App";
 
 // Client APIs
 export { apiScoutServer } from "./api/api-scout-server";
+export { apiScoutServerV1 } from "./api/api-scout-server-v1";
+export type { HeaderProvider } from "./api/api-scout-server-v1";
 
 // Client API - Types
 export type { ScanApi } from "./api/api.ts";

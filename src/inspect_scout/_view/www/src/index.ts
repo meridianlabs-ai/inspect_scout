c615c424db0a477fa33f0ea73d1f79adad763c1c
Eric Patey <eric.patey@gmail.com>
2026-02-03 15:09:59 -0500

    asdf

--- src/inspect_scout/_view/www/src/index.ts
+++ src/inspect_scout/_view/www/src/index.ts
@@ -2,11 +2,11 @@
 export { App } from "./App";
 
 // Client APIs
-export { apiScoutServer as apiScoutServer } from "./api/api-scout-server";
+export { apiScoutServer } from "./api/api-scout-server";
 export type { HeaderProvider } from "./api/api-scout-server";
 
 // Client API - Types
-export type { ScoutApiV2 as ScanApi } from "./api/api";
+export type { ScoutApiV2 } from "./api/api";
 
 // State Store
 export { ApiProvider, StoreProvider, createStore } from "./state/store";

8049d23553debea5d022f619c800642a3cf98a94
Eric Patey <eric.patey@gmail.com>
2026-02-08 19:06:17 -0500

    Regen openapi spec

--- src/inspect_scout/_view/www/src/types/generated.ts
+++ src/inspect_scout/_view/www/src/types/generated.ts
@@ -4005,6 +4005,10 @@ export interface components {
         };
         /** ValidationError */
         ValidationError: {
+            /** Context */
+            ctx?: Record<string, never>;
+            /** Input */
+            input?: unknown;
             /** Location */
             loc: (string | number)[];
             /** Message */

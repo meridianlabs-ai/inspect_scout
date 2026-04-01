4abd8a8147c7616bdf816c8c144142ea290cdc10
J.J. Allaire <jj.allaire@gmail.com>
2026-04-01 09:34:19 -0400

    improved forked_at detection for timeline branches

---
+++ src/inspect_scout/_view/dist/assets/index-BYzLViRA.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5ba49d71c740fc7a8b9f019de90af9853b9166d854827264cf5593d6f0c72a63
+size 3128627
--- src/inspect_scout/_view/dist/assets/index-C543mZ5l.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:cb81bb6fc33a0a9616a54cbfc1e4a3924dad5b6463ab9e0d79436c4ef526b8e9
-size 3128490

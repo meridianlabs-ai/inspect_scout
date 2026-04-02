0029a36f3b752b510abf15fed7922cd0fb5600f0
J.J. Allaire <jj.allaire@gmail.com>
2026-04-02 13:07:09 -0400

    use new timeline span branched_from

---
+++ src/inspect_scout/_view/dist/assets/index-D6ZjwctD.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:11e9992a2ed230217aed05cf6f04f1912a4f9c54c7bc9616627f93e37aa26922
+size 3128046
--- src/inspect_scout/_view/dist/assets/index-DJj9N_Gg.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2d441d88d58e5332d0d7c84c9fdb2feacbbc4c7cb81c9cfacfdb37ac9be614a4
-size 3128486

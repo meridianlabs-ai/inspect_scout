2dd9ca325094bf8ec3fe71f92bf970e43cf1f84a
Charles Teague <cteague@gmail.com>
2026-04-10 12:56:03 -0400

    gather latest

--- src/inspect_scout/_view/dist/assets/index-B0qi6Gke.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b595790dfc7d4993204016c420079d17ab81e2cad5bd6978627d5fed442f8a03
-size 3153748
---
+++ src/inspect_scout/_view/dist/assets/index-DnDJW9Ct.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c3f6ca3f4acac772c4bd301ab6a0301577d4a4ac811e08fea28d1227ffabba95
+size 3153842

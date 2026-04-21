8f39203f5418f48bea13d5acde9643ab97d3cf25
Matt Brandly <matt@meridianlabs.ai>
2026-04-21 15:14:19 -0400

    update git-lfs pointers

---
+++ src/inspect_scout/_view/dist/assets/tex-svg-full-BI3fonbT--VvH_qQa.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:98cfb9ec13aa76cf9dfec8e50574549690422fa0f7596dbd1ab8c62fa8d67698
+size 2233924
--- src/inspect_scout/_view/dist/assets/tex-svg-full-BI3fonbT-CmwvdOSY.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4a69243e6662e0cd95afd6dea7fb8080253092442e9c0f769eb3f32ac3611ed3
-size 2233916

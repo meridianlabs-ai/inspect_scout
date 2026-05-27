cbf0438befbbe13d2940fea4d3585e264b879212
Charles Teague <cteague@gmail.com>
2026-05-27 16:57:40 -0400

    Bump to latest (#456)
    * Bump to latest
    
    * update
    
    * bump to get types

---
+++ src/inspect_scout/_view/dist/assets/index-DxMU515C.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b83f6cd9128c274a4d409df242a59a48841c1015a899a18fd9432f639b960eca
+size 2684129
---
+++ src/inspect_scout/_view/dist/assets/wgxpath.install-node-Csk64Aj9-D9j-sZ5N.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2cd8f16fe336bf7949703537b77659dd5a9fae115ffdb826302cc987a0ac1486
+size 27972

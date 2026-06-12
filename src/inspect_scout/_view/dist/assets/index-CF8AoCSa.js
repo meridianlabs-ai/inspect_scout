f7217c8a218b36cbee9b6679c5e24c3cab3e4c2e
J.J. Allaire <jj.allaire@gmail.com>
2026-06-12 09:44:19 +0200

    update to latest ts-mono

--- src/inspect_scout/_view/dist/assets/index-CF8AoCSa.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c853622de2d5e7cb922db43121133f9af20c140089b42efbd02cbd08fbf89339
-size 2715442
---
+++ src/inspect_scout/_view/dist/assets/index-DlI7FR2y.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:478689007df54e45b29ebc547e69b845bd167375fb555ca695b5ca8ed99bd895
+size 2719318
